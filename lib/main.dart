3847b0c609f36ca3fac64058b88282155c6f4733
Engr. Mustafa <mustafarockz98@gmail.com>
2021-07-29 19:17:56 +0500

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:awesomeap/Pages/home_page.dart';
 import 'package:awesomeap/Pages/login_page.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,14 +14,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
+        "/home": (context) => LoginPage(),
         "/login": (context) => HomePage()
       },
     );
